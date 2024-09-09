b1244796d51577df2c2ae8526c3190adefdaaaec
Youngjoo <tyty4420@naver.com>
2024-09-09 13:35:45 +0900

    initial commit

---
+++ src/main/java/com/wanted/gold_room_corp/GoldRoomCorpApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.gold_room_corp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldRoomCorpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldRoomCorpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanted/gold_room_corp/GoldRoomCorpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.gold_room_corp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldRoomCorpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
